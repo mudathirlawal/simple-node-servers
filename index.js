65a98d543bc60b6fff09cea4783225b15f6bd1aa
Mudathir Lawal <nigercode@gmail.com>
2023-08-21 09:25:30 +0100

    feat: Modify server config

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const http = require("http");
-const port = 1003;
+const http = require( "http" );
+const port = 1200;
 const server = http.createServer();
 
-server.listen(port, () => {
-    console.log(`App has started; and server is 
-    listening for connection on port: ${port}.`
-    );
-});
+let str = `
+    App has started, and server is 
+    listening for connection on port: ${port}.
+`;
+
+server.listen( 
+    port, () => { console.log( str ); }
+);
 
 
 
