99222f7c91acbdeaa3a92a67498279f7542da4b8
Mudathir Lawal <nigercode@gmail.com>
2023-08-21 08:59:13 +0100

    feat: Add modules and configure server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log( "App has started; \nand server is listening on port 1003." );
+const http = require("http");
+const port = 1003;
+const server = http.createServer();
+
+server.listen(port, () => {
+    console.log(`App has started; and server is 
+    listening for connection on port: ${port}.`
+    );
+});
+
+
+
